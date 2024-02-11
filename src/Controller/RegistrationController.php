1209a94309e65410e7fd62607837c42a6ae65f71
JaysonMansuy34 <bwb.j.mansuy@fondespierre.com>
2024-02-11 17:35:05 +0100

    save finish authenticator full complet

--- src/Controller/RegistrationController.php
+++ src/Controller/RegistrationController.php
@@ -4,8 +4,12 @@ namespace App\Controller;
 
 use App\Entity\User;
 use App\Form\RegistrationFormType;
+use App\Repository\UserRepository;
 use App\Security\UsersAuthenticator;
+use App\Service\JWTService;
 use App\Service\SendMailService;
+use DateTimeImmutable;
+use Doctrine\ORM\EntityManager;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
@@ -29,7 +33,8 @@ class RegistrationController extends AbstractController
         UserAuthenticatorInterface $userAuthenticator,
         UsersAuthenticator $authenticator,
         EntityManagerInterface $entityManager,
-        SendMailService $email
+        SendMailService $email,
+        JWTService $jwt
     ): Response {
         $user = new User();
         $form = $this->createForm(RegistrationFormType::class, $user);
@@ -47,15 +52,35 @@ class RegistrationController extends AbstractController
             $entityManager->persist($user);
             $entityManager->flush();
             // do anything else you need here, like send an email
+
+            //On génère le jwt de l'utilisateur
+
+            //On crée le header
+            $header = [
+                'typ' => 'JWT',
+                'alg' => 'HS256'
+            ];
+
+            //On crée le payload
+            $payload = [
+                'id' => $user->getId()
+            ];
+
+            //On génère un token 
+            $token = $jwt->generate($header, $payload, $this->getParameter('app.jwtsecret'));
+
+
+
+
             //On envoie un mail
             $email->send(
                 'no-reply@test.fr',
                 $user->getUuid(),
                 'Activation de votre compte sur le site fablab',
                 'register',
-                    compact('user')
+                compact('user', 'token')
             );
-            
+
 
             return $userAuthenticator->authenticateUser(
                 $user,
@@ -67,5 +92,81 @@ class RegistrationController extends AbstractController
         return $this->render('registration/register.html.twig', [
             'registrationForm' => $form->createView(),
         ]);
+    }
+
+    #[Route('/verif/{token}', name: 'verify_user')]
+    public function verifyUser(
+        $token,
+        JWTService $jwt,
+        UserRepository $usersRepository,
+        EntityManagerInterface $em
+    ): Response {
+        //On verifie si le token valide, modifier ou expiré 
+        if (
+            $jwt->isValid($token)
+            && !$jwt->isExpired($token)
+            && $jwt->check($token, $this->getParameter('app.jwtsecret'))
+        ) {
+            //On recupere le payload
+            $payload = $jwt->getPlayLoad($token);
+
+            //on recupere le user du token
+            $user = $usersRepository->find($payload['id']);
+
+            //On verifie que l'utilisateur existe et n'a pas encore activié son compte 
+            if ($user && !$user->getIsVerified()) {
+                $user->setIsVerified(true);
+                $em->flush($user);
+                $this->addFlash("success", "Le token est activé");
+                return $this->redirectToRoute('app_accueil');
+            }
+        }
+        //Ici un problème se pose dans le token
+        $this->addFlash("danger", "Le token est invalide ou a expiré");
+        return $this->redirectToRoute('app_login');
+    }
+
+    #[Route('/renvoiverif', name: 'resend_verif')]
+    public function resendVerif(JWTService $jwt, SendMailService $mail, UserRepository $userRepository): Response
+    {
+        $user = $this->getUser();
+
+        if (!$user) {
+            $this->addFlash('danger', 'vous devez être connecté pour acceder à cette page');
+            return $this->redirectToRoute('app_login');
+        }
+
+        if ($user->getIsVerified()) {
+            $this->addFlash('warning', 'Cette utilisateur est déjà activé');
+            return $this->redirectToRoute('app_accueil');
+        }
+
+        //On crée le header
+        $header = [
+            'typ' => 'JWT',
+            'alg' => 'HS256'
+        ];
+
+        //On crée le payload
+        $payload = [
+            'id' => $user->getId()
+        ];
+
+        //On génère un token 
+        $token = $jwt->generate($header, $payload, $this->getParameter('app.jwtsecret'));
+
+
+
+
+        //On envoie un mail
+        $mail->send(
+            'no-reply@test.fr',
+            $user->getUuid(),
+            'Activation de votre compte sur le site fablab',
+            'register',
+            compact('user', 'token')
+        );
+        $this->addFlash('success', 'Email de verification envoyé');
+        return $this->redirectToRoute('app_accueil');
     }
 }
--- src/Service/JWTService.php
+++ src/Service/JWTService.php
@@ -1,4 +1,4 @@
-<?php 
+<?php
 
 namespace App\Service;
 
@@ -6,43 +6,103 @@ use DateTimeImmutable;
 
 class JWTService
 {
-    //On gènère un token
-
-    /** */
-    
+    /**
+     * generate du jwt
+     *
+     * @param  array $header
+     * @param  array $payload
+     * @param  string $secret
+     * @param  int $validity
+     * @return string
+     */
     public function generate(array $header, array $payload, string $secret, int $validity = 10800): string
     {
-        if($validity <= 0){
-            return "";
+        if ($validity > 0) {
+            $now = new DateTimeImmutable();
+            $exp = $now->getTimestamp() + $validity;
+
+            $payload['iat'] = $now->getTimestamp();
+            $payload['exp'] = $exp;
         }
 
-        $now = new DateTimeImmutable();
-        $exp = $now->getTimestamp() + $validity;
-
-        $payload['iat'] = $now->getTimestamp();
-        $payload['exp'] = $exp;
 
         //On encode tout en base64
-        $base64Header = base64_decode(json_encode($header));
-        $base64Payload = base64_decode(json_encode($payload));
+        $base64Header = base64_encode(json_encode($header));
+        $base64Payload = base64_encode(json_encode($payload));
 
         //Une fois "nettoie" les valeurs encodees (retrait des + / #)
-        $base64Header = str_replace(['+','/',"="], ['-','_',''], $base64Header);
-        $base64Payload = str_replace(['+','/',"="], ['-','_',''], $base64Payload);
+        $base64Header = str_replace(['+', '/', "="], ['-', '_', ''], $base64Header);
+        $base64Payload = str_replace(['+', '/', "="], ['-', '_', ''], $base64Payload);
 
         //Génère la signature 
         $secret = base64_encode($secret);
-        
+
         $signature = hash_hmac('sha256', $base64Header . '.' . $base64Payload, $secret, true);
 
         $base64Signature = base64_encode($signature);
 
-        $base64Signature = str_replace(['+','/',"="], ['-','_',''], $base64Signature);
+        $base64Signature = str_replace(['+', '/', "="], ['-', '_', ''], $base64Signature);
 
         //On crée le token 
 
         $jwt = $base64Header . '.' . $base64Payload . '.' . $base64Signature;
-        
+
         return $jwt;
+    }
+
+    // Verifier le token est valide (correctement formé)
+    public function isValid(string $token): bool
+    {
+        return preg_match(
+            '/^[a-zA-Z0-9\-\_\=]+\.[a-zA-Z0-9\-\_\=]+\.[a-zA-Z0-9\-\_\=]+$/',
+            $token
+        ) === 1;
+    }
+
+    //On  recuper payload 
+    public function getPlayLoad(string $token): array
+    {
+        // on separe le token 
+        $array = explode('.', $token);
+        //on decode le payload
+        $payload = json_decode(base64_decode($array[1]), true);
+
+        return $payload;
+    }
+
+    //On  recuper header 
+    public function getHeader(string $token): array
+    {
+        // on separe le token 
+        $array = explode('.', $token);
+        //on decode le header
+        $header = json_decode(base64_decode($array[0]), true);
+
+        return $header;
+    }
+
+    //Verifi si le token a expiré 
+    public function isExpired(string $token): bool
+    {
+        $payload = $this->getPlayLoad($token);
+
+        $now = new DateTimeImmutable();
+
+        return $payload['exp'] < $now->getTimestamp();
+    }
+
+    //On verifie  la signature du token
+
+    public function check(string $token, string $secret)
+    {
+        //On recuper le header et le payload
+        $header = $this->getHeader($token);
+        $payload = $this->getPlayLoad($token);
+
+
+        //regénère un token
+        $verifToken = $this->generate($header, $payload, $secret, 0);
+
+        return $token === $verifToken;
     }
 }
