327b0c5fec31e238ed056dcf010c508cd03aaae1
JaysonMansuy34 <bwb.j.mansuy@fondespierre.com>
2024-02-13 00:20:07 +0100

    Finish password reset

--- src/Controller/SecurityController.php
+++ src/Controller/SecurityController.php
@@ -2,9 +2,18 @@
 
 namespace App\Controller;
 
+use App\Form\ResetPasswordFormType;
+use App\Form\ResetPasswordRequestFormType;
+use App\Repository\UserRepository;
+use App\Service\SendMailService;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
 use Symfony\Component\Routing\Attribute\Route;
+use Symfony\Component\Routing\Generator\UrlGeneratorInterface;
+use Symfony\Component\Security\Csrf\TokenGenerator\TokenGeneratorInterface;
 use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
 
 class SecurityController extends AbstractController
@@ -16,11 +25,11 @@ class SecurityController extends AbstractController
      */
     #[Route(path: '/login', name: 'app_login')]
     public function login(AuthenticationUtils $authenticationUtils): Response
-    {   
+    {
         /**
          *  Condition de redirection si User est déjà connecté au cas ou qu'il accèdes à la page login
          */
-        
+
         if ($this->getUser()) {
             return $this->redirectToRoute('app_profil');
         }
@@ -32,8 +41,8 @@ class SecurityController extends AbstractController
 
         return $this->render('security/login.html.twig', [
             'last_username' => $lastUsername,
-             'error' => $error
-            ]);
+            'error' => $error
+        ]);
     }
 
     /**
@@ -41,10 +50,109 @@ class SecurityController extends AbstractController
      * @Route("/logout", name="app_logout")
      * La route permet de se deconnecter gérée directement par Symfony
      */
-    
+
     #[Route(path: '/logout', name: 'app_logout')]
     public function logout(): void
     {
         throw new \LogicException('This method can be blank - it will be intercepted by the logout key on your firewall.');
+    }
+
+    #[Route('/oublie-pass', name: 'forgotten_password')]
+    public function forgottenPassword(
+        Request $request,
+        UserRepository $userRepository,
+        TokenGeneratorInterface $tokenGenerator,
+        EntityManagerInterface $em,
+        SendMailService $email
+    ): Response {
+        $form = $this->createForm(ResetPasswordRequestFormType::class);
+
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            //On va chercher l'utilisateur par son email
+            $user = $userRepository->findOneByuuid($form->get('uuid')->getData());
+
+            //Verifier si on a un utilisateur
+            if ($user) {
+                // On génère un token de reinitialisation avec symfony
+                $token = $tokenGenerator->generateToken();
+                $user->setResetToken($token);
+                $em->persist($user);
+                $em->flush();
+
+                //On génère un lien de réinitialisation du password
+                $url = $this->generateUrl(
+                    'reset_pass',
+                    ['token' => $token],
+                    UrlGeneratorInterface::ABSOLUTE_URL
+                );
+
+                //On crée les donnes du mail
+                $context = compact('url', 'user');
+
+                //Envoi du mail
+                $email->send(
+                    'no-reply@hotmail.fr',
+                    $user->getUuid(),
+                    'Réinitilisation de mot de passe',
+                    'password_reset',
+                    $context
+                );
+
+                $this->addFlash('success', 'Email envoyez avec succès');
+                return $this->redirectToRoute('app_login');
+            }
+
+            //$user est null alors
+            $this->addFlash('danger', 'Un problème est survenu');
+            return $this->redirectToRoute('app_login');
+        }
+
+        return $this->render('security/reset_password_request.html.twig', [
+            'requestPassForm' => $form->createView()
+        ]);
+    }
+
+    #[Route('/oublie-pass/{token}', name: 'reset_pass')]
+    public function resetPass(
+        string $token,
+        Request $request,
+        UserRepository $userRepository,
+        EntityManagerInterface $em,
+        UserPasswordHasherInterface $passwordHasher): Response
+    {
+        //Verifier si on a ce token dans bdd
+        $user = $userRepository->findOneByResetToken($token);
+        
+        if($user){
+            $form = $this->createForm(ResetPasswordFormType::class);
+            
+            $form->handleRequest($request);
+            
+            if($form->isSubmitted() && $form->isValid()){
+                //On efface le token
+                $user->setResetToken('');
+                $user->setPassword(
+                    $passwordHasher->hashPassword(
+                        $user,
+                        $form->get('password')->getData()
+                    )
+                );
+                $em->persist($user);
+                $em->flush();
+
+                $this->addFlash('success', 'Mot de passe changé avec succès');
+                return $this->redirectToRoute('app_login');
+            }
+
+            return $this->render('security/reset_password.html.twig', [
+               'passForm' => $form->createView() 
+            ]);
+        }
+
+        $this->addFlash('danger', 'Jeton invalide');
+        return $this->redirectToRoute('app_login');
+
     }
 }
---
+++ src/Form/ResetPasswordRequestFormType.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Form;
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\EmailType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class ResetPasswordRequestFormType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+        ->add('uuid', EmailType::class,[
+            'attr' => [
+                'class' => 'form-control',
+                'placeholder' => 'exemple@email.fr'
+               ],
+            'label' => 'Entrez votre e-mail' 
+        ]);
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            // Configure your form options here
+        ]);
+    }
+}
