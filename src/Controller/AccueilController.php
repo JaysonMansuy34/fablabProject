a07fcc6fb04e7c719465cef5c51b7cc05bf09e6a
JaysonMansuy34 <bwb.j.mansuy@fondespierre.com>
2024-02-12 09:59:42 +0100

    Save create Profil html twig controller Role_user

--- src/Controller/AccueilController.php
+++ src/Controller/AccueilController.php
@@ -4,15 +4,36 @@ namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
-use Symfony\Component\Routing\Attribute\Route;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Security\Http\Attribute\IsGranted;
 
 class AccueilController extends AbstractController
-{
+{   
+    /**
+     * @Route Accueil (' / ', nom app_accueil)
+     * Page D'accueil
+     */
+    
     #[Route('/', name: 'app_accueil')]
     public function index(): Response
     {
         return $this->render('accueil/index.html.twig', [
             'controller_name' => 'AccueilController',
+        ]);
+    }
+
+    /**
+     * @Route accès Profil ('/profil', nom app_profil) 
+     * @Login obligatoire ROLE_USER
+     */
+
+    #[Route('/profil', name: 'app_profil')]
+    #[IsGranted('ROLE_USER')] // Appliquer la restriction de rôle ici
+    public function profil(): Response
+    {
+        // Utilisez 'getUser()' pour accéder à l'utilisateur connecté dans le template
+        return $this->render('accueil/profil.html.twig', [
+            'user' => $this->getUser(),
         ]);
     }
 }
